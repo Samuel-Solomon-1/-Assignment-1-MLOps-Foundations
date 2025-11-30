d39b27e642248adf8e133954e4b60c600a543b93
Samuel Solomon <solosamuel@gmail.com>
2025-11-30 20:03:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 
+
 app = Flask(__name__)
 model = joblib.load("model.pkl")
+
 
 @app.route("/")
 def home():
     return "Flask ML App is running!"
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -16,6 +18,7 @@ def predict():
     features = np.array(data["inputs"]).reshape(1, -1)
     prediction = model.predict(features).tolist()
     return jsonify({"prediction": prediction})
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
